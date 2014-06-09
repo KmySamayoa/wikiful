881cd35336b24b7ded36881182936b179d0e7ffd
KmySamayoa <camila.samayoa@gmail.com>
2014-06-09 14:05:29 +0000

    fixed application controller to article controller, added to view article index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,4 @@
 class ApplicationController < ActionController::Base
-  def index
-  end
-  
-  def show
-  end
-  
-  def new
-  end
-  
-  def create
-  end
   
   protect_from_forgery with: :exception
 end
